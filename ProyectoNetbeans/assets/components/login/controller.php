cece5f306f326d5335ac306cee8f54e390776eca
Inés Carazo <inescnunez@gmail.com>
2019-04-23 19:46:08 +0200

    Añadido archivo gitignore y modificado el contrlador dellogin

--- ProyectoNetbeans/assets/components/login/controller.php
+++ ProyectoNetbeans/assets/components/login/controller.php
@@ -14,8 +14,8 @@ function loginCorrecto($usuario, $contrasena)
     $modelClass = new modelClass();
     $result=$modelClass->comprobarLogin($usuario, $contrasena);
     if($result)
-    {
-        $url= 'http://localhost/ProyectoDAW/ProyectoNetbeans/assets/components/home/';
+    {          
+        $url= 'http://localhost/ProyectoDAW/ProyectoDAW/ProyectoNetbeans/assets/components/home/';
         header("Location: $url"); 
         echo "OK";
     }
