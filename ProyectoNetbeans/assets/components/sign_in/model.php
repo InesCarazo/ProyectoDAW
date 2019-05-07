b846881c8eb03b2e68f997e3dea8927b69433633
Inés <inescnunez@gmail.com>
2019-05-07 15:53:18 +0200

    Cambios en el model.php del registro y en el css del blog

--- ProyectoNetbeans/assets/components/sign_in/model.php
+++ ProyectoNetbeans/assets/components/sign_in/model.php
@@ -5,7 +5,11 @@ class modelClass{
         require_once './../conexion/conexion.php';
 
         try {
+            $conn->beginTransaction();
             $conn->exec("INSERT INTO usuario(usuario, contrasena, nombre, apellidos, telefono, correo, fechaNacimiento, rol) VALUES ('$usuario', MD5('$contrasena'),'$nombre','$apellidos',$telefono,'$correo','$fnacimiento','CLIENTE')");
+            $lastId = $conn->lastInsertId();
+            $conn->exec("INSERT INTO cliente(formaPago, nCuenta, A_usuario) VALUES ('', '', $lastId)");
+            $conn->commit();
         } catch (Exception $e) {
             $message = "Fallo: " . $e->getMessage();
             echo "<script type='text/javascript'>alert('$message');</script>";
